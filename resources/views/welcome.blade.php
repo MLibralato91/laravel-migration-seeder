87a66a2f5624c470db1b5a59aac077fc4b99ce8b
MLibralato91 <michelelibralato911@gmail.com>
2023-05-24 16:16:25 +0200

    create controller and create route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,10 @@
 @section('mainContent')
     <main>
         <h1>hello</h1>
+        <ul>
+            @foreach ($trains as $train)
+                <li>{{ $train->agency }}</li>
+            @endforeach
+        </ul>
     </main>
 @endsection
