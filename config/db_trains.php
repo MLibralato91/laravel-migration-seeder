fce26161c2486e51f713464cb9f379c9bfdb1c70
MLibralato91 <michelelibralato911@gmail.com>
2023-05-24 15:16:51 +0200

    first commit

---
+++ config/db_trains.php
@@ -0,0 +1,125 @@
+<?php
+
+$trains = [
+    [
+        'agency' => 'Trenitalia',
+        'departure_station' => 'Roma Termini',
+        'arrival_station' => 'Milano Centrale',
+        'departure_time' => '08:00',
+        'arrival_time' => '12:30',
+        'Codice Treno' => '12345',
+        'number_of_carriages' => 8,
+        'in_time' => 'Sì',
+        'deleted' => 'No'
+    ],
+    [
+        'agency' => 'Italo',
+        'departure_station' => 'Napoli Centrale',
+        'arrival_station' => 'Firenze Santa Maria Novella',
+        'departure_time' => '09:15',
+        'arrival_time' => '13:45',
+        'Codice Treno' => '54321',
+        'number_of_carriages' => 6,
+        'in_time' => 'No',
+        'deleted' => 'Sì'
+    ],
+    [
+        'agency' => 'Deutsche Bahn',
+        'departure_station' => 'Berlino Hauptbahnhof',
+        'arrival_station' => 'Monaco di Baviera Hauptbahnhof',
+        'departure_time' => '10:30',
+        'arrival_time' => '16:15',
+        'Codice Treno' => '98765',
+        'number_of_carriages' => 10,
+        'in_time' => 'Sì',
+        'deleted' => 'No'
+    ],
+    [
+        'agency' => 'SNCF',
+        'departure_station' => 'Parigi Gare du Nord',
+        'arrival_station' => 'Lione Part-Dieu',
+        'departure_time' => '11:45',
+        'arrival_time' => '16:30',
+        'Codice Treno' => '24680',
+        'number_of_carriages' => 5,
+        'in_time' => 'Sì',
+        'deleted' => 'No'
+    ],
+    [
+        'agency' => 'Renfe',
+        'departure_station' => 'Madrid Atocha',
+        'arrival_station' => 'Barcellona Sants',
+        'departure_time' => '13:00',
+        'arrival_time' => '17:45',
+        'Codice Treno' => '13579',
+        'number_of_carriages' => 9,
+        'in_time' => 'Sì',
+        'deleted' => 'No'
+    ],
+    [
+        'agency' => 'Renfe',
+        'departure_station' => 'Madrid Atocha',
+        'arrival_station' => 'Barcellona Sants',
+        'departure_time' => '13:00',
+        'arrival_time' => '17:45',
+        'Codice Treno' => '13579',
+        'number_of_carriages' => 9,
+        'in_time' => 'Sì',
+        'deleted' => 'No'
+    ],
+    [
+        'agency' => 'Amtrak',
+        'departure_station' => 'New York Penn Station',
+        'arrival_station' => 'Washington Union Station',
+        'departure_time' => '09:30',
+        'arrival_time' => '12:15',
+        'Codice Treno' => '24601',
+        'number_of_carriages' => 7,
+        'in_time' => 'Sì',
+        'deleted' => 'No'
+    ],
+    [
+        'agency' => 'JR Central',
+        'departure_station' => 'Tokyo Station',
+        'arrival_station' => 'Osaka Station',
+        'departure_time' => '11:00',
+        'arrival_time' => '15:30',
+        'Codice Treno' => '98765',
+        'number_of_carriages' => 12,
+        'in_time' => 'Sì',
+        'deleted' => 'No'
+    ],
+    [
+        'agency' => 'Eurostar',
+        'departure_station' => 'Londra St Pancras International',
+        'arrival_station' => 'Parigi Gare du Nord',
+        'departure_time' => '14:30',
+        'arrival_time' => '18:47',
+        'Codice Treno' => '75309',
+        'number_of_carriages' => 9,
+        'in_time' => 'Sì',
+        'deleted' => 'No'
+    ],
+    [
+        'agency' => 'RENFE-SNCF',
+        'departure_station' => 'Barcellona Sants',
+        'arrival_station' => 'Marsiglia Saint-Charles',
+        'departure_time' => '10:15',
+        'arrival_time' => '15:20',
+        'Codice Treno' => '35791',
+        'number_of_carriages' => 6,
+        'in_time' => 'Sì',
+        'deleted' => 'No'
+    ],
+    [
+        'agency' => 'ÖBB',
+        'departure_station' => 'Vienna Hauptbahnhof',
+        'arrival_station' => 'Salisburgo Hauptbahnhof',
+        'departure_time' => '13:45',
+        'arrival_time' => '15:30',
+        'Codice Treno' => '46820',
+        'number_of_carriages' => 4,
+        'in_time' => 'Sì',
+        'deleted' => 'No'
+    ]
+];
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('mainContent')
+    <main>
+        <h1>hello</h1>
+    </main>
+@endsection
